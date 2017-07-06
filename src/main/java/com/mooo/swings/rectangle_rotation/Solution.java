07312b56e5a4eecc1430af513fee1d4d37873a3b
Roel Goossens <roelgoossens22@hotmail.com>
2017-07-06 20:12:24 +0200

    Mother F :star: :star: :star: ing almost there

--- src/main/java/com/mooo/swings/rectangle_rotation/Solution.java
+++ src/main/java/com/mooo/swings/rectangle_rotation/Solution.java
@@ -3,10 +3,11 @@ package com.mooo.swings.rectangle_rotation;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.PathIterator;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Math.*;
 
 class Solution {
 
@@ -20,7 +21,7 @@ class Solution {
 
     private static Shape calculateRotatedRectangle(int a, int b) {
         Rectangle2D myRect = new Rectangle2D.Double(-a / 2, -b / 2, a, b);
-        AffineTransform at = AffineTransform.getRotateInstance(Math.PI / 4);
+        AffineTransform at = AffineTransform.getRotateInstance(PI / 4);
         return at.createTransformedShape(myRect);
     }
 
@@ -41,7 +42,7 @@ class Solution {
 
         tom.takeAHikeMate();
 
-        return 0;
+        return tom.getSuccessfulSteps();
     }
 
     private static void validateInput(int a, int b) {
@@ -50,46 +51,129 @@ class Solution {
         }
     }
 
-    private static class Hiker {
-        private final Point2D start;
-        private final Shape hikeArea;
-        private final String name;
+}
 
-        public Hiker(Shape hikeArea, String name) {
-            this.start = calculateStart();
-            this.hikeArea = hikeArea;
-            this.name = name;
+class Hiker {
+    private final Shape hikeArea;
+    private final String name;
+    private Point nextStep;
+    private Direction direction = Direction.DOWNISH;
+    private int failedSteps = 0;
+    private int successfulSteps = 0;
+    private boolean stop;
+    private Direction previousDirection = null;
+    Hiker(Shape hikeArea, String name) {
+        this.hikeArea = hikeArea;
+        this.nextStep = calculateStart();
+        this.name = name;
+    }
+
+    private static Point getNextPoint(PathIterator pathIterator) {
+        double[] cords = new double[2];
+        pathIterator.currentSegment(cords);
+        Point point = new Point((int) ceil(cords[0]), (int) floor(cords[1]));
+        pathIterator.next();
+        return point;
+    }
+
+    public int getSuccessfulSteps() {
+        return successfulSteps;
+    }
+
+    private Point calculateStart() {
+        List<Point> pointList = new ArrayList<>();
+        PathIterator pathIterator = hikeArea.getPathIterator(null);
+        for (int pointNumber = 0; pointNumber < 4; pointNumber++) {
+            pointList.add(getNextPoint(pathIterator));
         }
+        return pointList.get(2);// should be top point
+    }
 
-        private static Point2D.Double getNextPoint(PathIterator pathIterator) {
-            double[] cords = new double[2];
-            pathIterator.currentSegment(cords);
-            Point2D.Double point = new Point2D.Double(Math.ceil(cords[0]), Math.floor(cords[1]));
-            pathIterator.next();
-            return point;
-        }
+    void takeAHikeMate() {
+        while (!stop) {
+            Point currentStep = nextStep;
+            if (thisGoesNowhere()) {
+                failedSteps++;
+                currentStep = direction.showHikerBack(currentStep);
+                changeDirection(Direction.RIGHT);
+                System.out.println("Hiker " + name + " took a failed step,\n" +
+                        "They are at ( " + currentStep.x + " , " + currentStep.y + " ) right now,\n" +
+                        "They took " + failedSteps + " failed steps so far\n");
 
-        private Point2D calculateStart() {
-            List<Point2D.Double> pointList = new ArrayList<>();
-            PathIterator pathIterator = hikeArea.getPathIterator(null);
-            for (int pointNumber = 0; pointNumber < 4; pointNumber++) {
-                pointList.add(getNextPoint(pathIterator));
+                if (failedSteps > 1) {
+                    itsOver();
+                }
+            } else {
+                successfulSteps++;
+                failedSteps = 0;
+                System.out.println("Hiker " + name + " took a successful step,\n" +
+                        "They are at ( " + currentStep.x + " , " + currentStep.y + " ) right now,\n" +
+                        "They took " + successfulSteps + " successful steps so far\n");
+                changeDirectionIfGoingRight();
             }
-            return pointList.get(2);// should be top point
+            nextStep = direction.showHikerNextStep(currentStep);
         }
+    }
 
-        public void takeAHikeMate() {
-            Point2D nextStep = start;
+    private void changeDirection(Direction direction) {
+        previousDirection = this.direction;
+        this.direction = direction;
+    }
 
-            if (thisGoesSomewhere()) {
-
+    private void changeDirectionIfGoingRight() {
+        if (direction.equals(Direction.RIGHT)) {
+            if (previousDirection.equals(Direction.DOWNISH)) {
+                changeDirection(Direction.UPISH);
+            } else {
+                changeDirection(Direction.DOWNISH);
             }
-
         }
+    }
 
-        private boolean thisGoesSomewhere() {
-            return false;
-        }
+    private void itsOver() {
+        stop = true;
+    }
 
+    private boolean thisGoesNowhere() {
+        return !hikeArea.contains(nextStep);
+    }
+}
+
+class Direction {
+    static final Direction DOWNISH = new Direction(-1, -1);
+    static final Direction UPISH = new Direction(1, 1);
+    static final Direction RIGHT = new Direction(1, 0);
+    private final int xDirection;
+    private final int yDirection;
+
+    public Direction(int xDirection, int yDirection) {
+        this.xDirection = xDirection;
+        this.yDirection = yDirection;
+    }
+
+    public Point showHikerNextStep(Point currentStep) {
+        return new Point(currentStep.x + xDirection, currentStep.y + yDirection);
+    }
+
+    public Point showHikerBack(Point currentStep) {
+        return new Point(currentStep.x - xDirection, currentStep.y - yDirection);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Direction direction = (Direction) o;
+
+        if (xDirection != direction.xDirection) return false;
+        return yDirection == direction.yDirection;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = xDirection;
+        result = 31 * result + yDirection;
+        return result;
     }
 }
--- src/test/java/com/mooo/swings/rectangle_rotation/SolutionTest.java
+++ src/test/java/com/mooo/swings/rectangle_rotation/SolutionTest.java
@@ -10,11 +10,19 @@ import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
     @Test
-    public void BasicTests() {
+    public void BasicTest1() {
         assertEquals(65, Solution.rectangleRotation(30, 2));
+    }@Test
+    public void BasicTest2() {
         assertEquals(23, Solution.rectangleRotation(6, 4));
+    }@Test
+    public void BasicTest3() {
         assertEquals(49, Solution.rectangleRotation(8, 6));
+    }@Test
+    public void BasicTest4() {
         System.out.println( Solution.rectangleRotation(10000, 10000));
+    }@Test
+    public void BasicTest5() {
         assertEquals(333, Solution.rectangleRotation(16, 20));
     }
 }
