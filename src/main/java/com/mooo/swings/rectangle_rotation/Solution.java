c0be852dfa89c050c0b9e807ac9de07d1d269977
Roel Goossens <roelgoossens22@hotmail.com>
2017-07-06 18:54:22 +0200

    Going full :meat_on_bone: on this :poop:, Welcome to the world Hiker Tom

--- src/main/java/com/mooo/swings/rectangle_rotation/Solution.java
+++ src/main/java/com/mooo/swings/rectangle_rotation/Solution.java
@@ -1,66 +1,48 @@
 package com.mooo.swings.rectangle_rotation;
 
 import java.awt.*;
-import java.awt.geom.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
     static int rectangleRotation(final int a, final int b) {
         validateInput(a, b);
-        Rectangle2D myRect = new Rectangle2D.Double(-a / 2, -b / 2, a, b);
-        AffineTransform at = AffineTransform.getRotateInstance(Math.PI / 4);
-        Shape rotatedRect = at.createTransformedShape(myRect);
+        Shape rotatedRect = calculateRotatedRectangle(a, b);
 
         return calculatePoints(rotatedRect);
 //        return countPoints(rotatedRect);
     }
 
+    private static Shape calculateRotatedRectangle(int a, int b) {
+        Rectangle2D myRect = new Rectangle2D.Double(-a / 2, -b / 2, a, b);
+        AffineTransform at = AffineTransform.getRotateInstance(Math.PI / 4);
+        return at.createTransformedShape(myRect);
+    }
+
     /**
      * How about this:
-     *
-     * Find miny round to up move to first point
-     * move up util not in there
-     * move left (-x) (might be dependant on evenness off the thing or not ugh)
+     * <p>
+     * Find top point
+     * move down util not in there
+     * move right (x) (might be dependant on evenness off the thing or not ugh)
      * move up until not there
      * rinse and repeat until left is not there then u done?
-     *
+     * <p>
      * I hate this kata
-     *
      **/
 
     private static int calculatePoints(Shape rotatedRect) {
-        PathIterator pathIterator = rotatedRect.getPathIterator(null);
+        Hiker tom = new Hiker(rotatedRect, "Tom");
 
-        Point one = getNextPoint(pathIterator);
-        Point two = getNextPoint(pathIterator);
-        Point three = getNextPoint(pathIterator);
+        tom.takeAHikeMate();
 
-        double distance = one.distance(two);
-        double distance2 = two.distance(three);
-
-        int height = (int) distance;
-        int width = (int) distance2;
-        return calculatePoints(width, height , 0);
+        return 0;
     }
-
-    private static Point getNextPoint(PathIterator pathIterator){
-        double[] cords = new double[2];
-        pathIterator.currentSegment(cords);
-        Point point = new Point((int) Math.round(cords[0]),(int)Math.round(cords[1]));
-        pathIterator.next();
-        return point;
-    }
-
-    private static int calculatePoints(int width, int height , int points){
-        width--;
-        height--;
-        if (width <= 1 || height <= 1){
-            return points;
-        }
-        points += width * height;
-        return calculatePoints(width,height,points) ;
-    }
-
 
     private static void validateInput(int a, int b) {
         if (a > 10000 || b > 10000 || a < 2 || b < 2) {
@@ -68,35 +50,46 @@ class Solution {
         }
     }
 
-    private static int countPoints(Shape rotatedRect) {
-        int points;
-        points = getTopHalfPoints(rotatedRect) * 2;
-        points += getMiddlePoints(rotatedRect);
-        return points;
-    }
+    private static class Hiker {
+        private final Point2D start;
+        private final Shape hikeArea;
+        private final String name;
 
-    private static int getTopHalfPoints(Shape rotatedRect) {
-        int points = 0;
-        Rectangle bounds = rotatedRect.getBounds();
-        for (int x = (int) bounds.getMinX(); x < bounds.getMaxX(); x++) {
-            for (int y = 1; y < bounds.getMaxY(); y++) {
-                if (rotatedRect.contains(x, y)) {
-                    points++;
-                }
-            }
+        public Hiker(Shape hikeArea, String name) {
+            this.start = calculateStart();
+            this.hikeArea = hikeArea;
+            this.name = name;
         }
-        return points;
-    }
 
-    private static int getMiddlePoints(Shape rotatedRect) {
-        int points = 0;
-        Rectangle bounds = rotatedRect.getBounds();
-        for (int x = (int) bounds.getMinX(); x < (int) bounds.getMaxX(); x++) {
-            if (rotatedRect.contains(x, 0)) {
-                points++;
+        private static Point2D.Double getNextPoint(PathIterator pathIterator) {
+            double[] cords = new double[2];
+            pathIterator.currentSegment(cords);
+            Point2D.Double point = new Point2D.Double(Math.ceil(cords[0]), Math.floor(cords[1]));
+            pathIterator.next();
+            return point;
+        }
+
+        private Point2D calculateStart() {
+            List<Point2D.Double> pointList = new ArrayList<>();
+            PathIterator pathIterator = hikeArea.getPathIterator(null);
+            for (int pointNumber = 0; pointNumber < 4; pointNumber++) {
+                pointList.add(getNextPoint(pathIterator));
+            }
+            return pointList.get(2);// should be top point
+        }
+
+        public void takeAHikeMate() {
+            Point2D nextStep = start;
+
+            if (thisGoesSomewhere()) {
+
             }
 
         }
-        return points;
+
+        private boolean thisGoesSomewhere() {
+            return false;
+        }
+
     }
 }
