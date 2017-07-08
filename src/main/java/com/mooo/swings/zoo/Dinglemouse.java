3f8c959b0c92158c99d4c835e34f4342ef231079
Roel Goossens <roelgoossens22@hotmail.com>
2017-07-08 14:32:04 +0200

    Did some refactoring :+1:

--- src/main/java/com/mooo/swings/zoo/Dinglemouse.java
+++ src/main/java/com/mooo/swings/zoo/Dinglemouse.java
@@ -2,76 +2,127 @@ package com.mooo.swings.zoo;
 
 import java.util.*;
 
+import static java.util.Collections.singletonList;
+
 public class Dinglemouse {
 
-    public static String[] whoEatsWho(final String zoo) {
-        String[] foodStrings = zoo.split(",");
-        List<Food> foods = foodStringsToList(foodStrings);
-        return startFoodFest(zoo,foods);
+    public static String[] whoEatsWho(final String zooString) {
+
+        Zoo zoo = new Zoo(zooString);
+        return zoo.startFoodFest();
     }
 
-    private static String[] startFoodFest(String zoo, List<Food> foods) {
-        List<String> strings = new ArrayList<>();
-        strings.add(zoo);
-        for (int foodIndex = 0; foodIndex < foods.size(); foodIndex++) {
-            Food leftFood;
+}
 
-            Food rightFood;
-            try{
-                leftFood = foods.get(foodIndex - 1);
-            } catch (ArrayIndexOutOfBoundsException e){
-                leftFood = new Nothing();
-            }
-            try{
-                rightFood = foods.get(foodIndex + 1);
-            } catch (IndexOutOfBoundsException e){
-                rightFood = new Nothing();
-            }
-            String result = foods.get(foodIndex).eat(leftFood, rightFood);
-            Food whoGotEaten = whoWasEaten(result);
-            if (!whoGotEaten.equals(new Nothing())) {
+class Zoo {
 
-                if (whoGotEaten.equals(leftFood)){
-                    foods.remove(foodIndex -1 );
-                } else {
-                    foods.remove(foodIndex + 1 );
-                }
+    private final String zoo;
+    private List<Food> foods;
 
-                strings.add(result);
-                foodIndex = -1;
-            }
-        }
-        StringBuilder result = new StringBuilder();
-        for (Food food : foods) {
-            result.append(food.getName()).append(",");
-        }
-        result.deleteCharAt(result.length()-1);
-        strings.add(result.toString());
+    Zoo(String zoo) {
+        this.zoo = zoo;
+        this.foods = zooToFoodList(zoo);
+    }
+
+    String[] startFoodFest() {
+
+        List<String> strings = initializeStringsWithZoo();
+        foodFest(strings);
+        addFinalZooState(strings);
+
+        return listToStringArray(strings);
+    }
+
+    private ArrayList<String> initializeStringsWithZoo() {
+        return new ArrayList<>(singletonList(zoo));
+    }
+
+    private String[] listToStringArray(List<String> strings) {
         Object[] objects = strings.toArray();
         return Arrays.copyOf(objects, objects.length, String[].class);
     }
 
-    private static Food whoWasEaten(String result) {
+    private void foodFest(List<String> strings) {
+        for (int foodIndex = 0; foodIndex < foods.size(); foodIndex++) {
+            Food[] foodToEat = getFoodToEat(foodIndex);
+            Food eater = foods.get(foodIndex);
+
+            String result = eater.eat(foodToEat[0], foodToEat[1]);
+            if (removeEatenFood(result, foodToEat, foodIndex)) {
+                foodIndex = -1;
+                strings.add(result);
+            }
+        }
+    }
+
+    private void addFinalZooState(List<String> strings) {
+        StringBuilder result = new StringBuilder();
+        for (Food food : foods) {
+            result.append(food.getName()).append(",");
+        }
+        result.deleteCharAt(result.length() - 1);
+        strings.add(result.toString());
+    }
+
+    private boolean removeEatenFood(String result, Object[] foodToEat, int foodIndex) {
+        boolean somethingGotEaten = false;
+        Food whoGotEaten = whoWasEaten(result);
+        if (!whoGotEaten.equals(new Nothing())) {
+            if (whoGotEaten.equals(foodToEat[0])) {
+                foods.remove(foodIndex - 1);
+            } else {
+                foods.remove(foodIndex + 1);
+            }
+            somethingGotEaten = true;
+        }
+        return somethingGotEaten;
+    }
+
+    private Food[] getFoodToEat(int foodIndex) {
+        Food leftFood;
+
+        Food rightFood;
+        try {
+            leftFood = foods.get(foodIndex - 1);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            leftFood = new Nothing();
+        }
+        try {
+            rightFood = foods.get(foodIndex + 1);
+        } catch (IndexOutOfBoundsException e) {
+            rightFood = new Nothing();
+        }
+        return new Food[]{leftFood, rightFood};
+    }
+
+    private Food whoWasEaten(String result) {
         return Food.FOOD_REPOSITORY.get(result.split(" eats ")[1]);
     }
 
-    private static List<Food> foodStringsToList(String... foodStrings) {
+    private List<Food> zooToFoodList(String zoo) {
+        String[] foodStrings = zoo.split(",");
         List<Food> foods = new ArrayList<>();
         for (String foodString : foodStrings) {
             Food food = Food.FOOD_REPOSITORY.get(foodString);
-            if (food == null){
+            if (food == null) {
                 food = new Unknown(foodString);
             }
             foods.add(food);
         }
         return foods;
     }
-
 }
 
 abstract class Food {
 
     public final static Map<String, Food> FOOD_REPOSITORY = setupRepo();
+    private String name;
+    private List<Food> diet;
+
+    Food(String name, Food... diet) {
+        this.name = name;
+        this.diet = Arrays.asList(diet);
+    }
 
     private static Map<String, Food> setupRepo() {
         HashMap<String, Food> stringFoodHashMap = new HashMap<>();
@@ -91,14 +142,6 @@ abstract class Food {
         stringFoodHashMap.put("big-fish", new BigFish());
         stringFoodHashMap.put("nothing", new Nothing());
         return stringFoodHashMap;
-    }
-
-    private String name;
-    private List<Food> diet;
-
-    Food(String name, Food... diet) {
-        this.name = name;
-        this.diet = Arrays.asList(diet);
     }
 
     private boolean eats(Food food) {
@@ -235,11 +278,14 @@ class BigFish extends Food {
                 new LittleFish());
     }
 }
+
 class Nothing extends Food {
     Nothing() {
         super("nothing");
     }
-}class Unknown extends Food {
+}
+
+class Unknown extends Food {
     Unknown(String name) {
         super(name);
     }
