997ee5d1511f997e9988d0433e6fd6e3f5c149a9
Roel Goossens <roelgoossens22@hotmail.com>
2017-07-14 14:30:07 +0200

    Better test

--- src/test/java/com/mooo/swings/guesser/GuesserTest.java
+++ src/test/java/com/mooo/swings/guesser/GuesserTest.java
@@ -1,18 +1,36 @@
 package com.mooo.swings.guesser;
 
-import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.util.AssertionErrors;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by roel on 14/07/17.
  */
 public class GuesserTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @Test
     public void guess() throws Exception {
         Guesser.guess();
+        assertThat(outContent.toString()).contains("Correct");
     }
 }
